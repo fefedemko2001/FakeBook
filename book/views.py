a3a16b686828e48a42dfd8f689139b643f16e501
fefedemko2001 <fefedemko2001@gmail.com>
2024-01-18 10:54:32 +0100

    registration form

--- book/views.py
+++ book/views.py
@@ -1,21 +1,6 @@
 from django.shortcuts import render
 from .models import Post
 
-posts = [
-    {
-        'author':'Ferike',
-        'title':'Elso bejegy',
-        'content': 'valami',
-        'date_posted':'2024 01 08'
-    },
-    {
-        'author':'Anna',
-        'title':'Masodik bejegy',
-        'content': 'valami2',
-        'date_posted':'2024 01 10'
-    }
-]
-
 def home(request):
     context = {
         'posts': Post.objects.all()
