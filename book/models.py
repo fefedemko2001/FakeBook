fff4ad44c374d0948706d3f08e093ddfbe488275
fefedemko2001 <fefedemko2001@gmail.com>
2024-01-23 17:42:58 +0100

    post methods added

--- book/models.py
+++ book/models.py
@@ -1,12 +1,28 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     date_posted = models.DateTimeField(default = timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    images = models.ImageField(upload_to='post_images/', blank=True, null=True)
 
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse("post-detail", kwargs={"pk": self.pk})
+    
+    
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='post_images/')
+    description = models.CharField(max_length=255, blank=True, null=True)
+    post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='post_images')
+
+    def __str__(self):
+        return self.description or self.image.name
+    
