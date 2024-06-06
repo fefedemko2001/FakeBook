f039506208760d2be904b37d72ed02bf9d76ab23
fefedemko2001 <fefedemko2001@gmail.com>
2024-06-07 01:17:53 +0200

    minor updates

--- book/views.py
+++ book/views.py
@@ -62,8 +62,14 @@ class UserPostListView(ListView):
     context_object_name = 'posts'
 
     def get_queryset(self):
-        user = get_object_or_404(User, username=self.kwargs.get('username'))
-        return Post.objects.filter(author=user).order_by('-date_posted')
+        self.user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=self.user).order_by('-date_posted')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user_profile'] = self.user.profile
+        return context
+
 
 class PostDetailView(DetailView):
     model = Post
