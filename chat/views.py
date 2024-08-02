ebc8a9c8e5fbe9d5516409d355071db9a1469837
fefedemko2001 <fefedemko2001@gmail.com>
2024-08-02 15:05:57 +0200

    working chat

--- chat/views.py
+++ chat/views.py
@@ -24,6 +24,9 @@ def detail(request, username):
     user_profile = get_object_or_404(Profile, user=request.user)
     friend_profile = get_object_or_404(Profile, user__username=username)  
     chats = ChatMessage.objects.all()
+    received_chats = ChatMessage.objects.filter(sender = friend_profile, receiver = user_profile)
+    received_counter = received_chats.count()
+    received_chats.update(seen=True)
 
     form = ChatMessageFrom()
     if request.method == 'POST':
@@ -38,7 +41,8 @@ def detail(request, username):
         'user_profile': user_profile,
         'friend_profile': friend_profile,
         'form': form,
-        'chats': chats
+        'chats': chats,
+        'cnt': received_counter
     }
 
     return render(request, 'chat/detail.html', context)
@@ -48,6 +52,24 @@ def sentMessages(request, username):
     new_chat = data['msg']
     print(new_chat)
     sender = request.user.profile
-    friend = User.objects.get(username = username)
-    new_chat_message = ChatMessage.objects.create(body=new_chat, sender=sender, receiver=friend.profile, seen=False)
+    friend = User.objects.get(username = username).profile
+    new_chat_message = ChatMessage.objects.create(body=new_chat, sender=sender, receiver=friend, seen=False)
     return JsonResponse(new_chat_message.body, safe=False)
+
+def receivedMessages(request, username):
+    sender = User.objects.get(username = username).profile
+    receiver = request.user.profile
+    chatmessages = ChatMessage.objects.filter(sender = sender, receiver = receiver)
+    arr = []
+    for chat in chatmessages:
+        arr.append(chat.body)
+    return JsonResponse(arr, safe=False)
+
+def chatNotification(request):
+    user = request.user
+    friends = user.get_friends()
+    notifications = []
+    for friend in friends:
+        chats = ChatMessage.objects.filter(sender = friend.profile, receiver = request.user.profile , seen=False)
+        notifications.append(chats.count())
+    return JsonResponse(notifications, safe=False)
