c259d74a2645f2651ffc74b7c5fef4b1d6fce873
Demkó Ferenc <100278778+fefedemko2001@users.noreply.github.com>
2024-10-08 14:52:32 +0200

    Update views.py

--- users/views.py
+++ users/views.py
@@ -11,7 +11,7 @@ def register(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            messages.success(request, f'Your account has been created! You are now able to log in')
+            messages.success(request, f'Your account has been created! You are now able to log in.')
             return redirect('login')
     else:
         form = UserRegisterForm()
